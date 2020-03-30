213076f6a22796e85eb882b9f4e541082f7f56fe
gmmurray <trinilime123@live.com>
2020-03-29 23:59:24 -0400

    Updates

---
+++ precache-manifest.5c66b89d6c1d5c1a75271bead9809fa2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6163552f2e58b64d2fd92519d1470c39",
+    "url": "/covid19-near-me/index.html"
+  },
+  {
+    "revision": "50548b7e8c1453b4f91b",
+    "url": "/covid19-near-me/static/css/2.b9b2dd63.chunk.css"
+  },
+  {
+    "revision": "d00891c8f87cd2af55b9",
+    "url": "/covid19-near-me/static/css/main.15868c14.chunk.css"
+  },
+  {
+    "revision": "50548b7e8c1453b4f91b",
+    "url": "/covid19-near-me/static/js/2.2b545d59.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/covid19-near-me/static/js/2.2b545d59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d00891c8f87cd2af55b9",
+    "url": "/covid19-near-me/static/js/main.d4dae18a.chunk.js"
+  },
+  {
+    "revision": "bc3fde2d16445ad0cb9f",
+    "url": "/covid19-near-me/static/js/runtime-main.ad8a5bfc.js"
+  }
+]);
