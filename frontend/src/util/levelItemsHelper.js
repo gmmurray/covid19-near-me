bb0a257e9f6393cdf3935dd290762899aa05d8b3
gmmurray <trinilime123@live.com>
2020-04-18 23:52:31 -0400

    update to v2 for NovelCovid API and add extra data points

--- frontend/src/util/levelItemsHelper.js
+++ frontend/src/util/levelItemsHelper.js
@@ -1,11 +1,12 @@
 export const createStateItems = data => {
-	const { cases, todayCases, deaths, todayDeaths, active } = data;
+	const { cases, todayCases, deaths, todayDeaths, active, tests } = data;
 	return [
 		{ name: 'Cases', statistic: cases },
 		{ name: 'Cases Today', statistic: todayCases },
 		{ name: 'Deaths', statistic: deaths },
 		{ name: 'Deaths Today', statistic: todayDeaths },
 		{ name: 'Active', statistic: active },
+		{ name: 'Tests', statistic: tests },
 	];
 };
 
@@ -18,6 +19,7 @@ export const createCountryItems = data => {
 		recovered,
 		active,
 		critical,
+		tests,
 		casesPerOneMillion,
 		deathsPerOneMillion,
 	} = data;
@@ -30,18 +32,21 @@ export const createCountryItems = data => {
 		{ name: 'Recovered', statistic: recovered },
 		{ name: 'Active', statistic: active },
 		{ name: 'Critical', statistic: critical },
+		{ name: 'Tests', statistic: tests },
 		{ name: 'Cases/Million', statistic: casesPerOneMillion },
 		{ name: 'Deaths/Million', statistic: deathsPerOneMillion },
 	];
 };
 
 export const createWorldItems = data => {
-	const { cases, deaths, recovered, active } = data;
+	const { cases, deaths, recovered, active, tests, affectedCountries } = data;
 
 	return [
 		{ name: 'Cases', statistic: cases },
 		{ name: 'Deaths', statistic: deaths },
 		{ name: 'Recovered', statistic: recovered },
 		{ name: 'Active', statistic: active },
+		{ name: 'Tests', statistic: tests },
+		{ name: 'Affected Countries', statistic: affectedCountries },
 	];
 };
